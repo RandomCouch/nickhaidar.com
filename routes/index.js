29fe94e1890141ced506257c0c042fcca6f57349
Randomcouch <nicolasabouhaidar@gmail.com>
2017-03-28 17:11:10 +0000

    Sensitive data removed

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var React = require('react');
+var ReactDOM = require('react-dom/server');
+var DOM = React.DOM;
+var config = require('./config.js');
+const nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: config.username,
+        pass: config.pass
+    }
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', {title:"Nick Haidar", session: req.sess});
+    
+});
+router.post('/sendMail', function(req,res,next){
+   var email = req.body.email;
+   var message = req.body.message;
+   var actualMessage = "From " + email + ", \n" + message;
+   
+   console.log("Sending email from " + email + ": "  + message);
+   let mailOption = {
+       from: email,
+       to: "Target@Gmail.com",
+       subject: "Contact Message from NickHaidar.com",
+       text: actualMessage
+   };
+   transporter.sendMail(mailOption, (error,info) => {
+        if (error) {
+            console.log(error);
+        }
+        console.log("Message is sent : " + JSON.stringify(info));
+   });
+   res.send("Email sent");
+});
+
+module.exports = router;
